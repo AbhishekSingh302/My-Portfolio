9c5aabbcfe6561eae8a674e099668916dbddc83b
AbhishekSingh302 <143934196+AbhishekSingh302@users.noreply.github.com>
2023-09-17 12:44:39 +0530

    firebase commit

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -18,7 +18,7 @@ const WorkCard = () => {
                     <div className='pro-details'>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                         <div className='pro-btns'>
-                            <NavLink to="url" className="btn" target='_blank'>Click Here</NavLink>
+                            <NavLink to="https://my-portfolio-bb832.web.app" className="btn" target='_blank'>Click Here</NavLink>
                         </div>
                     </div>
                 </div>
